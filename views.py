726baf966c399685206526e676129de80dd08d0b
Esrat Jahan Limuza <esratjahanlimu2024@gmail.com>
2026-06-01 23:34:45 +0600

    Add files via upload

--- views.py
+++ views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
 
-# Create your views here.
+import django
+from django.shortcuts import redirect, render
+
+from assignments.models import About
+from blogs.models import blog, Category 
+from django.contrib.auth.forms import UserCreationForm
+from .forms import RegistrationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
+
+def home(request):
+    featured_posts = blog.objects.filter(is_featured=True, status='Published').order_by('-updated_at')
+    posts = blog.objects.filter(is_featured=False, status='Published')
+
+    #fetch about us
+    try:
+        about = About.objects.get()
+    except:
+        about = None
+    context = {
+        'featured_posts': featured_posts,
+        'posts': posts,
+        'about': about,
+
+    }
+    return render(request, 'home.html', context)
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('register')
+        else:
+            print(form.errors)
+    else:
+
+         form  = RegistrationForm()
+    context ={
+        'form': form,
+    }
+    return render(request, 'register.html', context)
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+            return redirect('dashboard')
+    form = AuthenticationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'login.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
